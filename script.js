0e6453193cb332f10dcabd78b4d7581e66717659
Sergio116-max <sergio.silvar@sansano.usm.cl>
2025-05-16 21:03:54 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("La página se ha cargado correctamente.");
+  
+  // Mostrar una alerta bienvenida (puedes quitarla si molesta)
+  alert("¡Bienvenido a nuestra página web!");
+});
